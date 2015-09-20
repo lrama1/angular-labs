0dc5c92ab8f0faab8dfa4d585f6b588abd4721ca
oleng <oleng_rama@hotmail.com>
2015-09-20 19:40:36 -0400

    added more code to building Angular routing

--- regex-lab/src/main/java/com/lab/RegExTester.java
+++ regex-lab/src/main/java/com/lab/RegExTester.java
@@ -1,5 +1,6 @@
 package com.lab;
 
+import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,31 +25,44 @@ public class RegExTester {
 			"	)();";
 
 			
-	public void test(){
+	public void appendNewRouteExpression(String newWhenExpression){
 		String whenRegex = "\\.when\\((.*?)\\)(\\;)*";
 		Pattern whenPattern = Pattern.compile(whenRegex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 		//String value = routePattern.matcher(routerString).
 		//System.out.println(value);
-		Matcher matcher = whenPattern.matcher(routerString);		
+		Matcher matcher = whenPattern.matcher(routerString);	
+		StringWriter whenWriter = new StringWriter();
 		//1.  gather all 'when' expressions
 		while(matcher.find()){
 			System.out.println("===========>" + matcher.group());
+			whenWriter.append("\n" + matcher.group());
 		}
 		
 		//2.  obtain the 'otherwise' expression (IF IT EXISTS)
 		String otherwiseRegex = "\\.otherwise\\((.*?)\\)(\\;)*";
 		Pattern otherwisePattern = Pattern.compile(otherwiseRegex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 		Matcher otherwiseMatcher = otherwisePattern.matcher(routerString);
+		StringWriter otherwiseWriter = new StringWriter();
 		while(otherwiseMatcher.find()){
-			System.out.println("OTHERWISE===========>" + otherwiseMatcher.group());
+			System.out.println("OTHERWISE EXPR===========>" + otherwiseMatcher.group());
+			otherwiseWriter.append(otherwiseMatcher.group());
 		}
+		
+		//append the new route to the list of 'WHEN' EXPRESSIONS 
+		whenWriter.append("\n" + newWhenExpression);
+		
+		System.out.println("=================================================================================");
+		System.out.println(whenWriter.toString());
 		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RegExTester regExTester = new RegExTester();
-		regExTester.test();
+		
+		String whenExprToAdd = ".when('/view3', {controller : 'VehicleController', templateUrl : '/test-web1/resources/js/angular_templates/view3.html'})";
+		
+		regExTester.appendNewRouteExpression(whenExprToAdd);
 	}
 
 }
